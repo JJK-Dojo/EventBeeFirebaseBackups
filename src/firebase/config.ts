67f3d8253c71e568813c794b88b107a2b46431a5
Alexa Skill <jjkalexaskill@gmail.com>
2026-01-08 08:53:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5253197355-71bc8",
+  "appId": "1:298250470686:web:e99a181b8e1e1518d1750f",
+  "apiKey": "AIzaSyDkRV_gPS-IUwY71TojtJTqwmvaMv1HEUA",
+  "authDomain": "studio-5253197355-71bc8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "298250470686"
+};
