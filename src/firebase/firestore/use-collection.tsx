5012c4353de9516e62c7e6ab1cab7c03778f51fc
Alexa Skill <jjkalexaskill@gmail.com>
2026-01-09 13:51:10 +0000

    Fix the error
    src/firebase/firestore/use-collection.tsx (109:11) @ mem

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -106,7 +106,7 @@ export function useCollection<T = any>(
       (error: FirestoreError) => {
         // This logic extracts the path from either a ref or a query
         const path: string =
-          memoizedTargetrefOrQuery.type === 'collection'
+          memoizedTargetRefOrQuery.type === 'collection'
             ? (memoizedTargetRefOrQuery as CollectionReference).path
             : (memoizedTargetRefOrQuery as unknown as InternalQuery)._query.path.canonicalString()
 
